62dc442b869547d75489bd2c901f5ecbc5786e1f
HaQi <luqmanananta745@gmail.com>
2025-05-18 15:53:32 +0700

    Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class authenticatable
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user';  // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
 
     public function level() : BelongsTo 
     {
